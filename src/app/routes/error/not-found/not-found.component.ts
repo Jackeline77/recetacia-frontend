21ad0b8d134a4c128d32df8c51b8a49d57314787
Eliana <you@example.com>
2025-11-17 14:51:06 -0500

    Loading

--- src/app/routes/error/not-found/not-found.component.ts
+++ src/app/routes/error/not-found/not-found.component.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { ImageCompareModule } from 'primeng/imagecompare';
+
 @Component({
   selector: 'app-not-found',
-  imports: [CommonModule, ButtonModule, CardModule],
+  imports: [CommonModule, ButtonModule, CardModule,ImageCompareModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
 })
